c0e1a358fb8547f3d509a60c3cb98e897cc798ad
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-02 21:52:05 +0000

    added basic setup files

---
+++ lib/environment.rb
@@ -0,0 +1,10 @@
+#example
+# require_relative "baseball/version"
+# require_relative "baseball/cli"
+# require_relative "baseball/game"
+# require_relative "baseball/scraper"
+
+
+# require 'pry'
+# require 'open-uri'
+# require 'nokogiri'
