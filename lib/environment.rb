087eb2d947b5d15e47105bc6dc7282e3dfde7f0b
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-03 03:10:14 +0000

    bin file,  environment, and gems configured

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,10 +1,13 @@
 #example
-# require_relative "baseball/version"
-# require_relative "baseball/cli"
-# require_relative "baseball/game"
-# require_relative "baseball/scraper"
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
 
 
-# require 'pry'
-# require 'open-uri'
-# require 'nokogiri'
+require_relative "taobites/cli/version"
+require_relative "taobites/cli"
+require_relative "taobites/scraper"
+
+
+
+
--- lib/taobites/cli.rb
+++ lib/taobites/cli.rb
@@ -1,8 +1,12 @@
-require "taobites/cli/version"
+#require "taobites/cli/version"
+#require 'pry'
 
 module Taobites
   module Cli
     class Error < StandardError; end
-    # Your code goes here...
+      #here code
+    puts "I am in cli.rb now"
+    # var = 0
+    # binding.pry 
   end
 end
