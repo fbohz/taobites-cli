641f3c0b4e24237cb2ad7250f82630c35adcc3f4
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-12 01:34:56 +0000

    ddj scraper and basic run defined

--- lib/taobites/scraper.rb
+++ lib/taobites/scraper.rb
@@ -1,12 +1,14 @@
+#require "./assets/taotechip.html"
+
 class Taobites::Scraper
  #doc = Nokogiri::HTML(open(""))
  #puts doc.css("#CSS-Selector-Here")
  
  
- #def self.temp
+ def self.new_ddj_bite
  chapter = rand(0..82)
   if chapter == 0 || chapter == 82
-    puts "Wow! You hit a byte! Lets see if you get lucky.."  
+    puts "\nWow! You hit a byte! Lets see if you get lucky.."  
     doc = Nokogiri::HTML(open("./assets/taotechip.html"))
     chapter_chip = rand(1..81)
     chapter_text = doc.css("##{chapter_chip}").text
@@ -15,16 +17,28 @@ class Taobites::Scraper
       sleep 1
       puts "..."
       sleep 2
-      puts "..."
-    puts "Chapter: #{chapter_chip}\n"
-    chapter_text
+      puts "...."
+    puts "Chapter: #{chapter_chip}\n" unless chapter_text.include?("Your lucky byte returned")
+    sleep 1
+    puts "#{chapter_text}\n"
   else 
     doc = Nokogiri::HTML(open("http://thetaoteching.com/taoteching#{chapter}.html"))
-    puts "Chapter: #{chapter}\n"
-    chapter_text = doc.css("td#loTBody").text
+      puts "Retrieving your Taobite.."
+      sleep 2
+      puts ".."
+      sleep 1
+      puts "..."
+      sleep 2
+      puts "...."
+    puts "Chapter: #{chapter}\n" #this should be taobite.ddj.chapter_num
+    sleep 1
+    chapter_text = doc.css("td#loTBody").text #this should be taobite.ddj.chapter_num
+    puts "#{chapter_text}\n"
     end   
-  #end 
- binding.pry
+  end 
+  
+  
+ #binding.pry
 end 
 
 #binding.pry
